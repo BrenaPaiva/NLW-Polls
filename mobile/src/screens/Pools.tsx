49f22ff0986e4d84208504f0b0b651dc91282e3a
Brena <bpaiva@uniaoquimica.com.br>
2023-04-24 18:46:44 -0300

    gitignore

--- mobile/src/screens/Pools.tsx
+++ mobile/src/screens/Pools.tsx
@@ -1,8 +1,15 @@
-import { VStack } from "native-base";
+import { VStack, Icon } from "native-base";
+import { Button } from "../components/Button";
+import { Header } from "../components/Header";
+import {Octicons} from "@expo/vector-icons"
 
 export function Pools() {
     return (
         <VStack flex={1} bgColor="gray.900">
+                <Header title="Meus bolões"/>
+            <VStack mt={6} mx={5} borderBottomWidth={1} borderBottomColor={"gray.600"} pb={4} mb={4}>
+                <Button title="BUSCAR BOLÃO POR CÓDIGO" leftIcon={<Icon as={Octicons} name="search" color="black" size="md" />}/>
+            </VStack>
             {/* parei em 1:40:43 */}
         </VStack>
     )
